605898c51eaf51f5afccb3f9e11d0d3d1f5b22e8
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-09-15 01:57:28 +0100

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -1,7 +1,26 @@
  int video;
+ typedef int size_t;
  
  
- 
+ // Função para contar o tamanho de uma string
+size_t lens(const char* str) {
+    size_t length = 0;
+    while (*str) {
+        length++;
+        str++;
+    }
+    return length;
+}
+
+// Função para copiar uma string e adicionar seu comprimento ao valor
+void strcpys(char* dest, const char* src) {
+    while (*src) {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0'; // Null-terminator
+}
  void printc(char b)
         {
         		   int i=video;
@@ -24,11 +43,12 @@ void prints(char *c)
  void kernel_main()
         {
         		   int c=0;
-        		   
+        		   char b[120];
 			   char *a="hello world.... ";
 			   video=0xb8000;
+			   strcpys(b,a);
 			   for(c=0;c<6;c++)
-			   prints(a);
+			   prints(b);
 			   
 			   
 			   	
