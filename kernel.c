1515bd553675c06991c617b725ea05f50c3100ea
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-09-15 02:17:29 +0100

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -1,7 +1,30 @@
  int video;
  typedef int size_t;
+ int NULL;
+ // Variável global para controlar o início da memória
+static unsigned char *memoryStart;
  
- 
+ int *malloc(int length) {
+    // Verifica se há espaço suficiente na memória
+    unsigned char *memoryEnd = memoryStart + length + sizeof(int) * 2;
+    if (memoryEnd > (unsigned char *)0x300000) {
+        // Não há espaço suficiente
+        return (int*)NULL;
+    }
+
+    // Preenche o bloco de memória com zeros
+    //memfill(memoryStart, length + sizeof(int) * 2, 0);
+
+    // Preenche o inteiro do cabeçalho
+    *(int *)memoryStart = length;
+
+    // Preenche o inteiro do rodapé
+    *(int *)(memoryStart + length + sizeof(int)) = length;
+
+    // Retorna o endereço após os inteiros do cabeçalho
+    return (int *)(memoryStart + sizeof(int));
+}
+
  // Função para contar o tamanho de uma string
 size_t lens(const char* str) {
     size_t length = 0;
@@ -43,9 +66,12 @@ void prints(char *c)
  void kernel_main()
         {
         		   int c=0;
-        		   char b[120];
+        		   char *b;
 			   char *a="hello world.... ";
 			   video=0xb8000;
+			   memoryStart = (unsigned char *)0x200000;
+			   NULL=0;
+			   b=(char*)malloc(1025);
 			   strcpys(b,a);
 			   for(c=0;c<6;c++)
 			   prints(b);
